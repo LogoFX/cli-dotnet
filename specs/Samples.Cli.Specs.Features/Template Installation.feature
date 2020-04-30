f2bed335d0322200c65841ad2875b79d8bf5e9b4
Gennady Verdel <godrose@gmail.com>
2020-04-30 14:29:47 +0300

    feat(spec-initial): added initial features - implementation pending

---
+++ specs/Samples.Cli.Specs.Features/Template Installation.feature
@@ -0,0 +1,10 @@
+﻿Feature: Template Installation
+	In order to provide template to developers who want to scaffold LogoFX-based solutions
+	As a framework developer
+	I want to be able to install the template via existing dotnet means
+
+Scenario: Install template
+	When I install the basics template via dotnet Cli
+	Then The template for 'LogoFX' is installed with the following parameters
+	| Templates     | Short Name    | Language | Tags       |
+	| LogoFX Basics | logofx-basics | C#       | LogoFX;WPF |
---
+++ specs/Samples.Cli.Specs.Steps/TemplateSteps.cs
@@ -0,0 +1,27 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Samples.Cli.Specs.Steps
+{
+    [Binding]
+    internal sealed class TemplateSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public TemplateSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [When(@"I install the basics template via dotnet Cli")]
+        public void WhenIInstallTheBasicsTemplateViaDotnetCli()
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Then(@"The template for '(.*)' is installed with the following parameters")]
+        public void ThenTheTemplateForIsInstalledWithTheFollowingParameters(string name, Table table)
+        {
+            _scenarioContext.Pending();
+        }
+    }
+}
