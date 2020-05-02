d4cc31e388b765f9fcd17e8d23aafc5bc3bc7b50
Dmitry Bublik <dmitry.buba@gmail.com>
2020-05-02 17:43:20 +0400

    feat: uninstall template by name, short name or directory name

--- utils/UninstallTemplate/Program.cs
+++ utils/UninstallTemplate/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 
 namespace UninstallTemplate
@@ -31,7 +33,7 @@ namespace UninstallTemplate
             }
 
             var name = string.Join(' ', args.Skip(1));
-            var lines = LaunchApp("dotnet", "new", "-u");
+            var lines = LaunchApp("dotnet new -u");
 
             var uninstallString = FindUninstallString(lines, name, kind);
 
@@ -40,20 +42,166 @@ namespace UninstallTemplate
                 return;
             }
 
-            var strings = uninstallString.Split(' ');
-            lines = LaunchApp(strings[0], strings.Skip(1).ToArray());
+            lines = LaunchApp(uninstallString);
             foreach (var line in lines)
             {
                 Console.WriteLine(line);
             }
         }
 
-        private static string FindUninstallString(string[] lines, string name, TemplateNameKind kind)
+        private static int GetIndents(string line)
         {
-            return @"dotnet new -u c:\Projects\LogoFX\cli-dotnet\templates\LogoFX.Templates.WPF";
+            var r = 0;
+            while (r < line.Length && line[r] == ' ')
+            {
+                r += 1;
+            }
+
+            return r;
         }
 
-        private static string[] LaunchApp(string appName, params string[] args)
+        private static int GetIndents(string[] lines, int index)
+        {
+            var line = lines[index];
+            return GetIndents(line);
+        }
+
+        private static string[] GetTemplates(string[] lines, ref int index)
+        {
+            var result = new List<string>();
+
+            while (index < lines.Length)
+            {
+                var line = lines[index];
+                index += 1;
+
+                if (GetIndents(line) == 4 &&
+                    string.Compare(line.Trim(), "Templates:", StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    while (index < lines.Length && GetIndents(lines, index) == 6)
+                    {
+                        result.Add(lines[index]);
+                        index += 1;
+                    }
+
+                    break;
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        private static string GetUninstallCommand(string[] lines, ref int index)
+        {
+            var result = new List<string>();
+
+            while (index < lines.Length)
+            {
+                var line = lines[index];
+                index += 1;
+
+                if (GetIndents(line) == 4 &&
+                    string.Compare(line.Trim(), "Uninstall Command:", StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    while (index < lines.Length && GetIndents(lines, index) == 6)
+                    {
+                        result.Add(lines[index].Trim());
+                        index += 1;
+                    }
+
+                    break;
+                }
+            }
+
+            return result.FirstOrDefault();
+        }
+
+        private static string GetNextSource(string[] lines, ref int index)
+        {
+            while (index < lines.Length && GetIndents(lines, index) != 2)
+            {
+                index += 1;
+            }
+
+            return index >= lines.Length ? null : lines[index++].Trim();
+        }
+
+        private static void ExtractName(string line, out string name, out string shortName, out string lang)
+        {
+            var brIndex1 = line.IndexOf('(');
+            var brIndex2 = line.IndexOf(')');
+
+            name = line.Substring(0, brIndex1).Trim();
+            shortName = line.Substring(brIndex1 + 1, brIndex2 - brIndex1 - 1).Trim();
+            lang = line.Substring(brIndex2 + 1).Trim();
+        }
+
+        private static string FindUninstallString(string[] lines, string name, TemplateNameKind kind)
+        {
+            var index = 0;
+            while (true)
+            {
+                var sourceName = GetNextSource(lines, ref index);
+                if (string.IsNullOrEmpty(sourceName))
+                {
+                    break;
+                }
+
+                if (kind == TemplateNameKind.Directory)
+                {
+                    var dirName = Path.GetFileName(sourceName);
+                    if (string.Compare(dirName, name, StringComparison.OrdinalIgnoreCase) != 0)
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    var templates = GetTemplates(lines, ref index);
+                    bool found = false;
+                    foreach (var template in templates)
+                    {
+                        ExtractName(template, out var name1, out var shortName, out var lang);
+                        string cmpName;
+                        switch (kind)
+                        {
+                            case TemplateNameKind.Name:
+                                cmpName = name1;
+                                break;
+                            case TemplateNameKind.ShortName:
+                                cmpName = shortName;
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException(nameof(kind), kind, null);
+                        }
+
+                        if (string.Compare(cmpName, name, StringComparison.OrdinalIgnoreCase) == 0)
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        continue;
+                    }
+                }
+
+                var uninstallCommand = GetUninstallCommand(lines, ref index);
+                return uninstallCommand;
+            }
+
+            return null;
+        }
+
+        private static string[] LaunchApp(string launchString)
+        {
+            var strings = launchString.Split(' ');
+            return LaunchApp(strings[0], strings.Skip(1).ToArray());
+        }
+
+        private static string[] LaunchApp(string appName, string[] args)
         {
             var process = new Process
             {
