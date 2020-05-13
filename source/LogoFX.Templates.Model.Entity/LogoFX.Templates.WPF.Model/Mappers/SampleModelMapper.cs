eba421ab289953849114e92e8cd2230f21c0a815
Dmitry Bublik <dmitry.buba@gmail.com>
2020-05-13 15:38:11 +0400

    feat: add mapper for newly added model

---
+++ source/LogoFX.Templates.Model.Entity/LogoFX.Templates.WPF.Model/Mappers/SampleModelMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using LogoFX.Templates.WPF.Data.Contracts.Dto;
+using LogoFX.Templates.WPF.Model.Contracts;
+
+namespace LogoFX.Templates.WPF.Model.Mappers
+{
+    internal sealed class SampleModelMapper
+    {
+        private readonly IMapper _mapper;
+
+        public SampleModelMapper(IMapper mapper) => _mapper = mapper;
+
+        public ISampleModel MapToCameraValue(SampleModelDto sampleModelDto) =>
+            _mapper.Map<ISampleModel>(sampleModelDto);
+    }
+}
