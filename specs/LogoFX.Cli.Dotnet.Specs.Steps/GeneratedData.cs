13f73d6029caf573eaed991b324ef08f82c4d40f
Gennady Verdel <godrose@gmail.com>
2020-05-15 09:25:32 +0300

    feat(assertion): improved assertion logic #46

--- specs/LogoFX.Cli.Dotnet.Specs.Steps/GeneratedData.cs
+++ specs/LogoFX.Cli.Dotnet.Specs.Steps/GeneratedData.cs
@@ -1,9 +1,14 @@
-﻿namespace LogoFX.Cli.Dotnet.Specs.Steps
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LogoFX.Cli.Dotnet.Specs.Steps
 {
     class GeneratedFolder
     {
         public string RootPath { get; }
         public string Name { get; }
+        public string FullName => Path.Combine(RootPath, Name);
 
         public GeneratedFolder(string rootPath, string name)
         {
@@ -11,8 +16,26 @@
             Name = name;
         }
 
-        public GeneratedFolder[] Folders { get; set; } = {};
-        public GeneratedFile[] Files { get; set; } = {};
+        public List<GeneratedFolder> Folders { get; set; } = new List<GeneratedFolder>();
+        public List<GeneratedFile> Files { get; set; } = new List<GeneratedFile>();
+
+        public GeneratedFolder WithFolder(string name, Func<GeneratedFolder, GeneratedFolder> updateFunc = null)
+        {
+            var folder = new GeneratedFolder(FullName,name);
+            if (updateFunc != null)
+            {
+                folder = updateFunc(folder);
+            }
+            Folders.Add(folder);
+            return this;
+        }
+
+        public GeneratedFolder WithFile(string name, string contents)
+        {
+            var file = new GeneratedFile(FullName, name, contents);
+            Files.Add(file);
+            return this;
+        }
     }
 
     class GeneratedFile
--- specs/LogoFX.Cli.Dotnet.Specs.Steps/ModelGenerationSteps.cs
+++ specs/LogoFX.Cli.Dotnet.Specs.Steps/ModelGenerationSteps.cs
@@ -7,37 +7,26 @@ namespace LogoFX.Cli.Dotnet.Specs.Steps
     internal sealed class ModelGenerationSteps
     {
         [Then(@"The folder '(.*)' contains generated model entity objects for solution name '(.*)'")]
-        public void ThenTheFolderContainsGeneratedModelEntityObjectsForSolutionName(string folderName, string solutionName)
+        public void ThenTheFolderContainsGeneratedModelEntityObjectsForSolutionName(string folderName,
+            string solutionName)
         {
             var tempPath = Path.GetTempPath();
             var path = Path.Combine(tempPath, folderName);
 
             //TODO: Improve building process to avoid duplication
             var structure = new GeneratedFolder(tempPath, folderName)
-                {
-                    Folders = new []
-                    {
-                        new GeneratedFolder(path, $"{solutionName}.Data.Contracts.Dto")
-                            {
-                                Files = new []{new GeneratedFile(Path.Combine(path, $"{solutionName}.Data.Contracts.Dto"), "SampleDto.cs", @"namespace Test.Data.Contracts.Dto
+                .WithFolder($"{solutionName}.Data.Contracts.Dto",
+                    r =>
+                        r.WithFile("SampleDto.cs", @"namespace Test.Data.Contracts.Dto
 {
     public class SampleDto
     {
 
     }
-}") }
-                        }
-                    }
-            };
-
+}"))
+                .WithFolder($"{solutionName}.Model")
+                .WithFolder($"{solutionName}.Model.Contracts");
             structure.AssertGeneratedCode();
-            //var subFolders = new[]
-            //{
-            //    $"{solutionName}.Data.Contracts.Dto",
-            //    $"{solutionName}.Model",
-            //    $"{solutionName}.Model.Contracts"
-            //};
-            //path.AssertSubFolders(subFolders);
         }
     }
 }
