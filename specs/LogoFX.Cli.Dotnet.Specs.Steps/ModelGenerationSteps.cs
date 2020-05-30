e4d16dfce61445eea5f66dca3729d4d53c5f4ee3
Gennady Verdel <godrose@gmail.com>
2020-05-30 08:05:35 +0300

    feat(model-assertion): added more cases for model assertion

--- specs/LogoFX.Cli.Dotnet.Specs.Steps/ModelGenerationSteps.cs
+++ specs/LogoFX.Cli.Dotnet.Specs.Steps/ModelGenerationSteps.cs
@@ -6,8 +6,9 @@ namespace LogoFX.Cli.Dotnet.Specs.Steps
     [Binding]
     internal sealed class ModelGenerationSteps
     {
-        [Then(@"The folder '(.*)' contains generated model entity objects for solution name '(.*)'")]
+        [Then(@"The folder '(.*)' contains generated model entity objects for name '(.*)' for solution name '(.*)'")]
         public void ThenTheFolderContainsGeneratedModelEntityObjectsForSolutionName(string folderName,
+            string entityName,
             string solutionName)
         {
             var tempPath = Path.GetTempPath();
@@ -15,112 +16,123 @@ namespace LogoFX.Cli.Dotnet.Specs.Steps
             var structure = new GeneratedFolder(tempPath, folderName)
                 .WithFolder($"{solutionName}.Data.Contracts.Dto",
                     r =>
-                        r.WithFile("SampleDto.cs", @"namespace Test.Data.Contracts.Dto
-{
-    public class SampleDto
-    {
+                        r.WithFile($"{entityName}Dto.cs", $@"namespace {solutionName}.Data.Contracts.Dto
+{{
+    public class {entityName}Dto
+    {{
 
-    }
-}")).WithFolder($"{solutionName}.Model.Contracts", r => r.WithFile("ISample.cs", @"namespace Test.Model.Contracts
-{
-    public interface ISample : IAppModel
-    {
+    }}
+}}")).WithFolder($"{solutionName}.Model.Contracts", r => r.WithFile($"I{entityName}.cs", $@"namespace {solutionName}.Model.Contracts
+{{
+    public interface I{entityName} : IAppModel
+    {{
         
-    }
-}"))
+    }}
+}}"))
                 .WithFolder($"{solutionName}.Model", r =>
-                    r.WithFile("Sample.cs", @"using Test.Model.Contracts;
+                    r.WithFile($"{entityName}.cs", $@"using {solutionName}.Model.Contracts;
 
-namespace Test.Model
-{
-    public class Sample : AppModel, ISample
-    {
+namespace {solutionName}.Model
+{{
+    public class {entityName} : AppModel, I{entityName}
+    {{
         
-    }
-}").WithFile("Module.cs", @"using System.Reflection;
+    }}
+}}").WithFile("Module.cs", $@"using System.Reflection;
 using AutoMapper;
 using JetBrains.Annotations;
-using Test.Model.Contracts;
-using Test.Model.Mappers;
+using {solutionName}.Model.Contracts;
+using {solutionName}.Model.Mappers;
 using Solid.Practices.IoC;
 using Solid.Practices.Modularity;
 
-namespace Test.Model
-{
+namespace {solutionName}.Model
+{{
     [UsedImplicitly]
     internal sealed class Module : ICompositionModule<IDependencyRegistrator>
-    {
+    {{
         public void RegisterModule(IDependencyRegistrator dependencyRegistrator)
-        {
+        {{
             dependencyRegistrator
                 .RegisterAutomagically(
                     Assembly.LoadFrom(AssemblyInfo.AssemblyName),
                     Assembly.GetExecutingAssembly());
 
             var config = new MapperConfiguration(cfg =>
-            {
+            {{
                 cfg.AddProfile(new MappingProfile());
-            });
+            }});
             var mapper = config.CreateMapper();
             dependencyRegistrator
                 .AddInstance(mapper)
-                .AddSingleton<SampleMapper>();
-        }
-    }
-}
+                .AddSingleton<{entityName}Mapper>();
+        }}
+    }}
+}}
 ").WithFolder("Mappers", s => 
-                        s.WithFile("MappingProfile.cs", @"using System;
+                        s.WithFile("MappingProfile.cs", $@"using System;
 using AutoMapper;
-using Test.Data.Contracts.Dto;
-using Test.Model.Contracts;
+using {solutionName}.Data.Contracts.Dto;
+using {solutionName}.Model.Contracts;
 
-namespace Test.Model.Mappers
-{
+namespace {solutionName}.Model.Mappers
+{{
     internal sealed class MappingProfile : Profile
-    {
+    {{
         public MappingProfile()
-        {
+        {{
             CreateSampleItemMaps();
-            CreateSampleMaps();
-        }
+            Create{entityName}Maps();
+        }}
 
-        private void CreateSampleMaps()
-        {
-            CreateDomainObjectMap<SampleDto, ISample, Sample>();
-        }
+        private void Create{entityName}Maps()
+        {{
+            CreateDomainObjectMap<{entityName}Dto, I{entityName}, {entityName}>();
+        }}
 
         private void CreateSampleItemMaps()
-        {
+        {{
             CreateDomainObjectMap<SampleItemDto, ISampleItem, SampleItem>();
-        }
+        }}
 
         private void CreateDomainObjectMap<TDto, TContract, TModel>()
             where TModel : TContract where TContract : class => CreateDomainObjectMap(typeof(TDto), typeof(TContract), typeof(TModel));
         private void CreateDomainObjectMap(Type dtoType, Type contractType, Type modelType)
-        {
+        {{
             CreateMap(dtoType, contractType).As(modelType);
             CreateMap(dtoType, modelType);
             CreateMap(contractType, dtoType);
             CreateMap(modelType, dtoType);
-        }
-    }
-}").WithFile("SampleMapper.cs", @"using AutoMapper;
-using Test.Data.Contracts.Dto;
-using Test.Model.Contracts;
+        }}
+    }}
+}}").WithFile($"{entityName}Mapper.cs", $@"using AutoMapper;
+using {solutionName}.Data.Contracts.Dto;
+using {solutionName}.Model.Contracts;
 
-namespace Test.Model.Mappers
-{
-    internal sealed class SampleMapper
-    {
+namespace {solutionName}.Model.Mappers
+{{
+    internal sealed class {entityName}Mapper
+    {{
         private readonly IMapper _mapper;
 
-        public SampleMapper(IMapper mapper) => _mapper = mapper;
+        public {entityName}Mapper(IMapper mapper) => _mapper = mapper;
 
-        public ISample MapToSampleValue(SampleDto sampleModelDto) =>
-            _mapper.Map<ISample>(sampleModelDto);
-    }
-}")));
+        public I{entityName} MapTo{entityName}Value({entityName}Dto {Decapitalize(entityName)}ModelDto) =>
+            _mapper.Map<I{entityName}>({Decapitalize(entityName)}ModelDto);
+    }}
+}}")));
             structure.AssertGeneratedCode();
+        }
+
+        private string Decapitalize(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return str;
+            }
+
+            var firstChar = char.ToLowerInvariant(str[0]);
+            return str.Length > 1 ? firstChar + str.Substring(1) : firstChar.ToString();
         }
     }
 }
