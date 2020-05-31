40727f72167dc6f8b93a44ba6affb56c43697985
Gennady Verdel <godrose@gmail.com>
2020-05-31 14:31:21 +0300

    feat(assertion): added suppport for Date.Fake.ProviderBuilders

--- specs/LogoFX.Cli.Dotnet.Specs.Steps/SolutionGenerationSteps.cs
+++ specs/LogoFX.Cli.Dotnet.Specs.Steps/SolutionGenerationSteps.cs
@@ -193,13 +193,79 @@ namespace {folderName}.Data.Fake.Containers
     }}   
 }}
 "))
+                //TODO: Consider adding csproj as well
                 .WithFolder($"{folderName}.Data.Fake.Containers.Contracts", r=> r.WithFile("IDataContainer.cs", $@"namespace {folderName}.Data.Fake.Containers.Contracts
 {{
     public interface IDataContainer
     {{
     }}
 }}"))
-                .WithFolder($"{folderName}.Data.Fake.ProviderBuilders")
+                //TODO: Consider adding csproj as well
+                .WithFolder($"{folderName}.Data.Fake.ProviderBuilders", r => r.WithFile("SampleProviderBuilder.cs", $@"using System;
+using System.Collections.Generic;
+using System.Linq;
+using Attest.Fake.Builders;
+using Attest.Fake.Core;
+using Attest.Fake.Setup.Contracts;
+using {folderName}.Data.Contracts.Dto;
+using {folderName}.Data.Contracts.Providers;
+
+namespace {folderName}.Data.Fake.ProviderBuilders
+{{
+    public sealed class SampleProviderBuilder : FakeBuilderBase<ISampleProvider>.WithInitialSetup
+    {{
+        private readonly List<SampleItemDto> _itemsStorage = new List<SampleItemDto>();
+
+        private SampleProviderBuilder()
+        {{
+
+        }}
+
+        public static SampleProviderBuilder CreateBuilder() => new SampleProviderBuilder();
+
+        public void WithItems(IEnumerable<SampleItemDto> items)
+        {{
+            _itemsStorage.Clear();
+            _itemsStorage.AddRange(items);
+        }}
+
+        protected override IServiceCall<ISampleProvider> CreateServiceCall(
+            IHaveNoMethods<ISampleProvider> serviceCallTemplate) => serviceCallTemplate
+            .AddMethodCallWithResult(t => t.GetItems(),
+                r => r.Complete(GetItems))
+            .AddMethodCallWithResult<Guid, bool>(t => t.DeleteItem(It.IsAny<Guid>()),
+                (r, id) => r.Complete(DeleteItem(id)))
+            .AddMethodCallWithResult<SampleItemDto, bool>(t => t.UpdateItem(It.IsAny<SampleItemDto>()),
+                (r, dto) => r.Complete(k =>
+                {{
+                    SaveItem(k);
+                    return true;
+                }}))
+            .AddMethodCall<SampleItemDto>(t => t.CreateItem(It.IsAny<SampleItemDto>()),
+                (r, dto) => r.Complete(SaveItem));
+
+        private IEnumerable<SampleItemDto> GetItems() => _itemsStorage;
+
+        private bool DeleteItem(Guid id)
+        {{
+            var dto = _itemsStorage.SingleOrDefault(x => x.Id == id);
+            return dto != null && _itemsStorage.Remove(dto);
+        }}
+
+        private void SaveItem(SampleItemDto dto)
+        {{
+            var oldDto = _itemsStorage.SingleOrDefault(x => x.Id == dto.Id);
+            if (oldDto == null)
+            {{
+                _itemsStorage.Add(dto);
+                return;
+            }}
+
+            oldDto.DisplayName = dto.DisplayName;
+            oldDto.Value = dto.Value;
+        }}
+    }}
+}}"))
                 .WithFolder($"{folderName}.Data.Fake.Providers")
                 .WithFolder($"{folderName}.Data.Real.Providers")
                 .WithFolder($"{folderName}.Launcher")
