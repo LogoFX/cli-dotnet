66c0374cf2aefb62d7e4e928173db921ca234406
Dmitry Bublik <dmitry.buba@gmail.com>
2020-06-02 22:33:00 +0400

    feat: add template for data provider contract #75

---
+++ templates/LogoFX.Templates.Service.Entity/LogoFX.Templates.Data.Contracts.Providers/ISampleModelDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using LogoFX.Templates.Data.Contracts.Dto;
+
+namespace LogoFX.Templates.Data.Contracts.Providers
+{
+    public interface ISampleModelDataProvider
+    {
+        IEnumerable<SampleModelDto> GetItems();
+        
+        bool DeleteItem(string id);
+        
+        bool UpdateItem(SampleModelDto dto);
+        
+        void CreateItem(SampleModelDto dto);
+    }
+}
