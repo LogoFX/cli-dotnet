21d7bf58835846ec17aeffe70d3e7786040779d6
Dmitry Bublik <dmitry.buba@gmail.com>
2020-06-03 23:40:29 +0400

    feat: create template for fake data provider #78

--- specs/LogoFX.Cli.Dotnet.Specs.Steps/ServiceGenerationSteps.cs
+++ specs/LogoFX.Cli.Dotnet.Specs.Steps/ServiceGenerationSteps.cs
@@ -230,6 +230,57 @@ namespace {solutionName}.Data.Fake.ProviderBuilders
             }}
         }}
     }}
+}}")).WithFolder($"{solutionName}.Data.Fake.Providers",
+                    r => r.WithFile($"Fake{entityName}DataProvider.cs",
+                        $@"using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Attest.Fake.Builders;
+using JetBrains.Annotations;
+using {solutionName}.Data.Contracts.Dto;
+using {solutionName}.Data.Contracts.Providers;
+using {solutionName}.Data.Fake.Containers;
+using {solutionName}.Data.Fake.ProviderBuilders;
+
+namespace {solutionName}.Data.Fake.Providers
+{{
+    [UsedImplicitly]
+    internal sealed class Fake{entityName}DataProvider : FakeProviderBase<{entityName}ProviderBuilder, I{entityName}DataProvider>, I{entityName}DataProvider
+    {{
+        private readonly Random _random = new Random();
+
+        public Fake{entityName}DataProvider(
+            {entityName}ProviderBuilder sampleProviderBuilder,
+            I{entityName}DataContainer sampleContainer)
+            : base(sampleProviderBuilder)
+        {{
+            sampleProviderBuilder.WithItems(sampleContainer.Items);
+        }}
+
+        IEnumerable<{entityName}Dto> I{entityName}DataProvider.GetItems() => GetService(r =>
+        {{
+            Task.Delay(_random.Next(2000)).Wait();
+            return r;
+        }}).GetItems();
+
+        bool I{entityName}DataProvider.DeleteItem(Guid id) => GetService(r =>
+        {{
+            Task.Delay(_random.Next(2000)).Wait();
+            return r;
+        }}).DeleteItem(id);
+
+        bool I{entityName}DataProvider.UpdateItem({entityName}Dto dto) => GetService(r =>
+        {{
+            Task.Delay(_random.Next(2000)).Wait();
+            return r;
+        }}).UpdateItem(dto);
+
+        void I{entityName}DataProvider.CreateItem({entityName}Dto dto) => GetService(r =>
+        {{
+            Task.Delay(_random.Next(2000)).Wait();
+            return r;
+        }}).CreateItem(dto);
+    }}
 }}"));
             structure.AssertGeneratedCode();
         }
---
+++ templates/LogoFX.Templates.Service.Entity/LogoFX.Templates.Data.Fake.Providers/FakeSampleModelDataProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Attest.Fake.Builders;
+using JetBrains.Annotations;
+using LogoFX.Templates.Data.Contracts.Dto;
+using LogoFX.Templates.Data.Contracts.Providers;
+using LogoFX.Templates.Data.Fake.Containers;
+using LogoFX.Templates.Data.Fake.ProviderBuilders;
+
+namespace LogoFX.Templates.Data.Fake.Providers
+{
+    [UsedImplicitly]
+    internal sealed class FakeSampleModelDataProvider : FakeProviderBase<SampleModelProviderBuilder, ISampleModelDataProvider>, ISampleModelDataProvider
+    {
+        private readonly Random _random = new Random();
+
+        public FakeSampleModelDataProvider(
+            SampleModelProviderBuilder sampleProviderBuilder,
+            ISampleModelDataContainer sampleContainer)
+            : base(sampleProviderBuilder)
+        {
+            sampleProviderBuilder.WithItems(sampleContainer.Items);
+        }
+
+        IEnumerable<SampleModelDto> ISampleModelDataProvider.GetItems() => GetService(r =>
+        {
+            Task.Delay(_random.Next(2000)).Wait();
+            return r;
+        }).GetItems();
+
+        bool ISampleModelDataProvider.DeleteItem(Guid id) => GetService(r =>
+        {
+            Task.Delay(_random.Next(2000)).Wait();
+            return r;
+        }).DeleteItem(id);
+
+        bool ISampleModelDataProvider.UpdateItem(SampleModelDto dto) => GetService(r =>
+        {
+            Task.Delay(_random.Next(2000)).Wait();
+            return r;
+        }).UpdateItem(dto);
+
+        void ISampleModelDataProvider.CreateItem(SampleModelDto dto) => GetService(r =>
+        {
+            Task.Delay(_random.Next(2000)).Wait();
+            return r;
+        }).CreateItem(dto);
+    }
+}
