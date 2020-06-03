87606fde3ac1d83c155bda7aae4a8c48f94a1cec
Dmitry Bublik <dmitry.buba@gmail.com>
2020-06-03 22:40:39 +0400

    feat: add tests for data service implementation

--- specs/LogoFX.Cli.Dotnet.Specs.Features/Service Generation.feature
+++ specs/LogoFX.Cli.Dotnet.Specs.Features/Service Generation.feature
@@ -3,6 +3,37 @@
 	As an app developer
 	I want to be able to install and use the correspondent template via existing dotnet means
 
+Scenario Outline: Generate model entity in an empty folder
+	When I install the template pack 'LogoFX.Templates' from local package
+	And I create a folder named 'Generation'
+	And I generate the code in folder named 'Generation' using 'logofx-model' template with the following options
+	| Name                 | Value             |
+	| -n                   | <entityNameValue> |
+	| <solutionNameOption> | Test              |
+	| --allow-scripts      | yes               |
+	Then The folder 'Generation' contains generated model entity objects for name '<entityNameValue>' for solution name 'Test'
+
+	Examples:
+	| entityNameValue | solutionNameOption |
+	| Sample          | -sn                |
+	| Another         | --solution-name    |
+
+Scenario Outline: Generate model entity in existing solution
+	When I install the template pack 'LogoFX.Templates' from local package
+	And I create a folder named 'Generation'
+	And I generate the code in folder named 'Generation' using 'logofx-wpf' template with the default options
+	And I generate the code in folder named 'Generation' using 'logofx-model' template with the following options
+	| Name                 | Value  |
+	| -n                   | Sample |
+	| <solutionNameOption> | Test   |
+	| --allow-scripts      | yes    |
+	Then The folder 'Generation' contains working LogoFX template-based solution
+	And The folder 'Generation' contains generated model entity objects for name 'Sample' for solution name 'Test'
+
+	Examples:
+	| solutionNameOption |
+	| -sn                |
+	| --solution-name    |
 
 Scenario Outline: Generate service entity in an empty folder
 	When I install the template pack 'LogoFX.Templates' from local package
--- specs/LogoFX.Cli.Dotnet.Specs.Steps/ServiceGenerationSteps.cs
+++ specs/LogoFX.Cli.Dotnet.Specs.Steps/ServiceGenerationSteps.cs
@@ -51,6 +51,68 @@ namespace {solutionName}.Model.Contracts
 
         Task DeleteItem(I{entityName} item);
     }}
+}}")).WithFolder($"{solutionName}.Model",
+                    r => r.WithFile($"{entityName}Service.cs",
+                        $@"using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using LogoFX.Client.Core;
+using LogoFX.Core;
+using {solutionName}.Data.Contracts.Providers;
+using {solutionName}.Model.Contracts;
+using {solutionName}.Model.Mappers;
+
+namespace {solutionName}.Model
+{{
+    [UsedImplicitly]
+    internal sealed class {entityName}Service : NotifyPropertyChangedBase<{entityName}Service>, I{entityName}Service
+    {{
+        private readonly I{entityName}DataProvider _provider;
+        private readonly {entityName}Mapper _mapper;
+
+        private readonly RangeObservableCollection<I{entityName}> _items =
+            new RangeObservableCollection<I{entityName}>();
+
+        public {entityName}Service(I{entityName}DataProvider provider, {entityName}Mapper mapper)
+        {{
+            _provider = provider;
+            _mapper = mapper;
+        }}
+
+        private void GetItems()
+        {{
+            var items = _provider.GetItems().Select(_mapper.MapTo{entityName});
+            _items.Clear();
+            _items.AddRange(items);
+        }}
+
+        IEnumerable<I{entityName}> I{entityName}Service.Items => _items;
+
+        Task I{entityName}Service.GetItems() => MethodRunner.RunAsync(GetItems);
+
+        Task<I{entityName}> I{entityName}Service.NewItem() => MethodRunner.RunWithResultAsync<I{entityName}>(() => new {entityName}());
+
+        Task I{entityName}Service.SaveItem(I{entityName} item) => MethodRunner.RunAsync(() =>
+        {{
+            var dto = _mapper.MapTo{entityName}Dto(item);
+
+            if (item.IsNew)
+            {{
+                _provider.CreateItem(dto);
+            }}
+            else
+            {{
+                _provider.UpdateItem(dto);
+            }}
+        }});
+
+        Task I{entityName}Service.DeleteItem(I{entityName} item) => MethodRunner.RunAsync(() =>
+        {{
+            _provider.DeleteItem(item.Id);
+            _items.Remove(item);
+        }});
+    }}
 }}"));
 
             structure.AssertGeneratedCode();
