cf5176cb8c244a4aa6fc57079b6b24147599c17c
Gennady Verdel <godrose@gmail.com>
2020-06-12 13:16:33 +0300

    feat(template-names): renamed 'LogoFX.Templates.Service.Entity' to 'LogoFX.Templates.Service' #92

--- devops/copy-template-logofx-service.cmd
+++ devops/copy-template-logofx-service.cmd
@@ -1,12 +1,12 @@
 REM copy-template-logofx-service.cmd 
 
-call copy-folder LogoFX.Templates.Service.Entity /exclude:..\devops\exclude-common.txt+..\devops\exclude-for-service.txt
+call copy-folder LogoFX.Templates.Service /exclude:..\devops\exclude-common.txt+..\devops\exclude-for-service.txt
 
 if %ERRORLEVEL% NEQ 0 (
 	goto EXIT
 )
 
-call generate-utils.cmd LogoFX.Templates.Service.Entity service-utils
+call generate-utils.cmd LogoFX.Templates.Service service-utils
 
 :EXIT
 REM /copy-template-logofx-service.cmd 
